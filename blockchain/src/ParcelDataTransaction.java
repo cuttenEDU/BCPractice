6252cdd37248702e1a20d039c70a032a568300ef
cuttenEDU <z.zhmakin@gmail.com>
2019-07-10 23:25:26 +0300

    added signature to transactions & blocks, added blockchain, added demo functionality

--- blockchain/src/ParcelDataTransaction.java
+++ blockchain/src/ParcelDataTransaction.java
@@ -1,20 +1,24 @@
 import com.google.common.hash.HashCode;
 
+import javax.xml.bind.DatatypeConverter;
+import java.security.PublicKey;
 import java.sql.Time;
 
 public class ParcelDataTransaction implements Transaction {
     private String parcelTN;
-    private String senderPublicKey;
+    private PublicKey senderPublicKey;
+    private byte[] signature;
     private String data;
     private long timestamp;
     private HashCode hashCode;
 
-    public ParcelDataTransaction(String parcelTN, String senderPublicKey, String data, long timestamp) {
+    public ParcelDataTransaction(String parcelTN, PublicKey senderPublicKey, String data, long timestamp, byte[] signature) {
         this.parcelTN = parcelTN;
         this.senderPublicKey = senderPublicKey;
         this.data = data;
         this.timestamp = timestamp;
-        this.hashCode = Hasher.hash(senderPublicKey+parcelTN+data);
+        this.hashCode = Hasher.hash(senderPublicKey + parcelTN + data);
+        this.signature = signature;
     }
 
 
@@ -22,9 +26,29 @@ public class ParcelDataTransaction implements Transaction {
         return parcelTN;
     }
 
-    String getInfo(){
-        return "Hashcode: " + hashCode.toString() + "\tPublic key: " + senderPublicKey  + "\tParcel TN: " + parcelTN + "\tData: " + data + "\tTimestamp: " + timestamp;
+    public HashCode getHashCode() {
+        return hashCode;
+    }
+
+    String getInfo() {
+        return "Hashcode: " + hashCode.toString() + "\tPublic key: " + Hasher.bytesToHex(senderPublicKey.getEncoded()) + "\tParcel TN: " + parcelTN + "\tData: " + data + "\tTimestamp: " + timestamp + "\tSignature: " + DatatypeConverter.printHexBinary(signature);
+    }
+
+    String getTruncInfo() {
+        return "Hashcode: " + truncate(hashCode.toString()) + "\tPublic key: " + truncate(Hasher.bytesToHex(senderPublicKey.getEncoded())) + "\tParcel TN: " + parcelTN + "\tData: " + data + "\tTimestamp: " + timestamp + "\tSignature: " + truncate(DatatypeConverter.printHexBinary(signature));
+
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    String truncate(String string) {
+        return string.substring(0, 6) + "..." + string.substring(string.length() - 7, string.length() - 1);
     }
 
 
 }
+
+
+
